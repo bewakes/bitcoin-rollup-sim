904f6a1c24f87a63214606f9c975e942aeaf3b8b
Bibek Pandey <bewakepandey@gmail.com>
2023-09-15 23:55:05 +0545

    Create pay_to function for wallet

--- bitcoin_rollup_sim/node.py
+++ bitcoin_rollup_sim/node.py
@@ -1,13 +1,14 @@
 import random
-from typing import Literal, TypeAlias, List
+import hashlib
 from dataclasses import dataclass
+from fastecdsa import ecdsa, curve
 
-from .transaction import Transaction
+from .transaction import Transaction, VIn, VOut
 from .peers import send_transaction
 from .validation import validate_transaction
 from .utils.node import get_inputs_for
 
-NodeRole: TypeAlias = Literal["miner", "wallet", "fullnode", "spvnode"]
+# NodeRole: TypeAlias = Literal["miner", "wallet", "fullnode", "spvnode"]
 
 
 @dataclass
@@ -22,8 +23,6 @@ class Node:
         self.nid = f"{self.type}-{random.randrange(10000)}"
 
     def propagate_transaction(self, transaction: Transaction):
-        from .global_state import get_node
-
         self.propagated_txns.append(transaction.txid)
 
         for peer_id in self.peers:
@@ -52,11 +51,51 @@ class WalletNode(Node):
     For simplicity wallet node handles just a single address
     """
     priv_key: int
-    pub_key: str
+    pub_key: int
+    address: str
     type = "wallet"
+
+    def create_signature(self, txn: Transaction, vout_ind: int) -> str:
+        # TODO: I am not exacly sure what to sign. So I'll just sign
+        # the serialized vout indicated by vout_ind
+        vout = txn.vout[vout_ind]
+        vout_ser = vout.serialize()
+        hashed = hashlib.sha256(vout_ser.encode("utf-8")).digest()
+        r, s = ecdsa.sign(hashed, self.priv_key, curve.secp256k1)
+        # NOTE: In real, DER encoding is done, but I simplify here as below
+        return f"{r}:{s}"
 
     def pay_to(self, amt_sats: int, addr: str):
         inps = get_inputs_for(self.pub_key, amt_sats)
+        if not inps:
+            print(f"{self.nid} - Insufficient Funds({amt_sats} sats)!")
+            return
+        surplus = sum([x[2] for x in inps]) - amt_sats
+
         # Create txn
-        # Add txn pool
-        # Propagate
+        vins = [
+            VIn(
+                transaction_id=inp[0].txid,
+                vout=inp[1],
+                coinbase="",
+                script_sig=" ".join([
+                    self.create_signature(inp[0], inp[1]),
+                    str(self.pub_key),  # TODO: maybe hash
+                ]),
+                sequence=1,  # TODO: what to do here?
+            )
+            for inp in inps
+        ]
+        vouts = [
+            VOut.get_for_p2pk(addr, amt_sats),
+        ]
+        if surplus:  # pay back to yourself
+            vouts.append(VOut.get_for_p2pk(self.address, surplus))
+
+        txn = Transaction.new(vins, vouts)
+
+        # Add to txn pool
+        self.txn_pool.append(txn.txid)
+
+        # And then propagate
+        self.propagate_transaction(txn)
--- bitcoin_rollup_sim/utils/node.py
+++ bitcoin_rollup_sim/utils/node.py
@@ -1,24 +1,36 @@
-from typing import List, Tuple
+from typing import List, Tuple, Literal, TypeAlias, Union
 
 from bitcoin_rollup_sim.transaction import VIn, Transaction
 from bitcoin_rollup_sim.global_state import get_utxos
 
 
-def check_can_spend(utxo: Transaction, pub_key: str) -> Tuple[bool, int | None]:
-    # TODO: complete
-    return True, 0
+def check_can_spend(utxo: Transaction, pub_key: int) -> int | None:
+    ...
 
 
-def get_inputs_for(pub_key: str, amt_sats: int) -> List[VIn]:
+VoutInd: TypeAlias = int
+Amt: TypeAlias = int
+
+
+def get_inputs_for(
+    pub_key: int,
+    amt_sats: int
+) -> List[Tuple[Transaction, VoutInd, Amt]]:
     """
     This scans unspent txns that can be spent by pub_key
     """
     utxos: list[Transaction] = get_utxos()
-    # TODO: get from UTXO set
     sum_amt = 0
+    items: List[Tuple[Transaction, VoutInd, Amt]] = []
     for utxo in utxos:
         # Check vout for each
-        can_spend, vout_ind = check_can_spend(utxo, pub_key)
-        if can_spend:
-            sum_amt += utxo.vout[vout_ind].value
+        vout_ind = check_can_spend(utxo, pub_key)
+        if vout_ind is not None:
+            val = utxo.vout[vout_ind].value
+            sum_amt += val
+            items.append((utxo, vout_ind, val))
+            if sum_amt >= amt_sats:
+                return items
+    # If it reaches here, then the amount cannot be satisfied from pubkey
+    # spendable utxos
     return []
